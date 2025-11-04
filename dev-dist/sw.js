27bdeb5e810594d93d9963e64eb10c09188ea966
Pavlo Chabanov <businesseuropolicy@gmail.com>
2025-11-04 16:59:36 -0500

    0.8.2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-cb96c7ab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qu5da184058"
+    "revision": "0.h9s48m52h7"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
