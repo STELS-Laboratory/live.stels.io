85ab0b97d6edaee79f330abea2201a506b183b14
Pavlo Chabanov <businesseuropolicy@gmail.com>
2025-11-13 17:53:03 -0500

    0.9.4.2 Developer Access Modal Close (FIX)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-cb96c7ab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7o5k5375l6o"
+    "revision": "0.gneenv2mceg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
